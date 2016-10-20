9d33ae33859b2fe44db366b8dcb9a30c585fba48
Tomas Barton <barton.tomas@gmail.com>
2016-10-20 15:36:45 +0200

    print messages only in verbose mode

--- csv2arff/csv2arff.py
+++ csv2arff/csv2arff.py
@@ -19,13 +19,15 @@ class Csv2Arff():
         self.write_arff()
 
     def read_csv(self):
-        print('Reading csv file to convert arff file')
+        if self.verbose():
+            print("Reading CSV file '%s'" % (self.args.input))
         data = np.genfromtxt(self.args.input, delimiter=',', dtype='str')
         self.columns = data[0]
         self.data = np.array(data[1:])
 
     def determine_attribute_types(self):
-        print('Calculating attribute types')
+        if self.verbose():
+            print('Calculating attribute types')
         for (i, attribute) in enumerate(self.columns):
             unique = list(set(self.data[:, i]))
 
@@ -47,12 +49,17 @@ class Csv2Arff():
                     column_data[data_index] = "'%s'" % str(value)
 
                 self.data[:, i] = column_data
+        if self.verbose():
+            print('Found %i attributes' % (len(self.columns)))
 
     def write_arff(self):
-        print('Writing as arff file')
+        if self.verbose():
+            print("Writing ARFF to '%s' file" % (self.args.output))
+
         new_file = open(self.args.output, 'w')
 
         # name from CLI arguments
+        name = ''
         if hasattr(self.args, 'name'):
             name = self.args.name
         else:
@@ -64,7 +71,7 @@ class Csv2Arff():
                 name = self.args.output
 
         # Write relation
-        new_file.write('@relation ' + name + '\n\n')
+        new_file.write('@relation ' + str(name) + '\n\n')
 
         # Write attributes
         for column in self.columns:
@@ -81,6 +88,12 @@ class Csv2Arff():
         new_file.write('@data\n')
         new_file.write('\n'.join(lines))
         new_file.close()
+
+    def verbose(self):
+        if hasattr(self.args, 'verbose') and self.args.verbose:
+            return True
+        else:
+            return False
 
     def is_numeric(self, lit):
         'Return value of numeric literal string or ValueError exception'
@@ -118,7 +131,8 @@ class Csv2Arff():
 def main():
     parser = argparse.ArgumentParser(prog='csv2arff')
     parser.add_argument('-n', '--name', help='ARFF relation name')
-    parser.add_argument('-v', action='store_true', help="verbose output")
+    parser.add_argument('-v', '--verbose', action='store_true',
+                        help="verbose output")
     parser.add_argument('input', help='input CSV file name')
     parser.add_argument('output', help='output ARFF file name')
     args = parser.parse_args()
